4d747af1a8738a2103f1b555ab165ad40ae8088b
Saren <ykmran@gmail.com>
2014-06-13 05:27:48 +0800

    Unused

--- src/main/java/net/wtako/WTAKOUtils/Commands/Wut/ArgHelp.java
+++ src/main/java/net/wtako/WTAKOUtils/Commands/Wut/ArgHelp.java
@@ -10,8 +10,6 @@ public class ArgHelp {
     public ArgHelp(CommandSender sender) {
         sender.sendMessage(Main.getInstance().getName() + " v" + Main.getInstance().getProperty("version"));
         sender.sendMessage("Author: " + Main.getInstance().getProperty("author"));
-        sender.sendMessage(Lang.HELP_OFF.toString());
-        sender.sendMessage(Lang.HELP_TSH.toString());
         sender.sendMessage(Lang.HELP_RELOAD.toString());
     }
 
