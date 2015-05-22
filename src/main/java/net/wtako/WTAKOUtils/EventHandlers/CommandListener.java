62091e1ec72e4abc0fa32c78e26f315603938699
Saren Arterius <saren@wtako.net>
2015-05-22 23:36:30 +0800

    omg

--- src/main/java/net/wtako/WTAKOUtils/EventHandlers/CommandListener.java
+++ src/main/java/net/wtako/WTAKOUtils/EventHandlers/CommandListener.java
@@ -4,6 +4,7 @@ import net.wtako.WTAKOUtils.Main;
 import net.wtako.WTAKOUtils.Utils.Config;
 import net.wtako.WTAKOUtils.Utils.Lang;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
@@ -11,15 +12,13 @@ import java.text.MessageFormat;
 
 public class CommandListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onCommandCalled(PlayerCommandPreprocessEvent event) {
-        System.out.println("Event called");
         if (event.getPlayer().hasPermission(Main.artifactId + ".banned-commands-exempt")) {
             return;
         }
         try {
             String playerCommand = event.getMessage().split(" ")[0].substring(1);
-            System.out.println(playerCommand);
             Config.BANNED_COMMANDS.getStrings().stream().filter(bannedCommand -> bannedCommand.equalsIgnoreCase(playerCommand)).forEach(bannedCommand -> {
                 event.setCancelled(true);
                 event.getPlayer().sendMessage(Lang.TITLE.toString() + MessageFormat.format(Lang.COMMAND_BANNED.toString(), playerCommand));
